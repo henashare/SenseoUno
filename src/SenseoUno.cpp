b751d22fc9473814d5acf965f68448d6ffce9230
lucasdaub <lucasdaubrez@gmail.com>
2021-03-31 01:54:17 +0200

    Fin modifications méthodes chronos

--- src/SenseoUno.cpp
+++ src/SenseoUno.cpp
@@ -379,21 +379,19 @@ void SenseoUno::startChrono(long countdown_value){
 }
 
 bool SenseoUno::isElapsedChrono1(volatile int *val){
-	/*if(*val>=counter1){
+	if(*val>=counter1){
 		*val = 0;
 		return 1;
 	}
-	return 0;*/
-	return (*val>counter1) ? 1 : 0;
+	return 0;
 }
 
 bool SenseoUno::isElapsedChrono2(volatile int *val){
-	/*if(*val>=counter2){
+	if(*val>=counter2){
 		*val = 0;
 		return 1;
 	}
-	return 0;*/
-	return (*val>counter2) ? 1 : 0;
+	return 0;
 }
 
 void SenseoUno::rebootChrono(volatile int *val){
